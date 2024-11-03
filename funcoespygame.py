3e71872614e60a6c8ca271a457abb6b167280cad
Maira Gentil <mairajgs@al.insper.edu.br>
2024-11-02 21:27:41 -0300

    Início

---
+++ funcoespygame.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((900, 600))
+pygame.display.set_caption('Fireboy and Watergirl')
+
+game = True
+while game == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+    window.fill((255,255,255)) #fundo branco
+    pygame.display.update()
+
+pygame.quit()
