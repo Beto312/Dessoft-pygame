f712d9329b87adc1063d0be6005ee3585df7924a
Beto312 <robertomb4@al.insper.edu.br>
2024-11-04 10:40:35 -0300

    tela inicial

--- funcoespygame.py
+++ funcoespygame.py
@@ -2,13 +2,14 @@ import pygame
 
 pygame.init()
 
-largura = 1500
+largura = 900
 altura = 775
 
 window = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Fireboy and Watergirl')
-telainicial = pygame.image.load("---.png")
-telainicial = imagem_fundo = pygame.transform.scale(telainicial,(largura, altura))
+telainicial = pygame.image.load("assets\img\inicial.png").convert()
+rect = telainicial.get_rect()
+imagem_fundo = pygame.transform.scale(telainicial,(largura, altura))
 
 
 game = True
@@ -16,7 +17,10 @@ while game == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-    window.fill((255,255,255)) #fundo branco
+    window.fill((255,255,255))
+    window.blit(imagem_fundo,rect) #fundo do jogo
     pygame.display.update()
+
+    
 
 pygame.quit()
