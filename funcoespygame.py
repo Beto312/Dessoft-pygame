6b34f1f3ae00c4abf429e11d20f8e5d19cc8616e
Beto312 <robertomb4@al.insper.edu.br>
2024-11-04 10:14:20 -0300

    aa

--- funcoespygame.py
+++ funcoespygame.py
@@ -2,16 +2,16 @@ import pygame
 
 pygame.init()
 
-info = pygame.display.Info()
-window = pygame.display.set_mode((info.current_w, info.current_h))
+window = pygame.display.set_mode((900, 600))
 pygame.display.set_caption('Fireboy and Watergirl')
 
+
 game = True
-while game:
+while game == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-    window.fill((255, 255, 255))  # fundo branco
+    window.fill((255,255,255)) #fundo branco
     pygame.display.update()
 
 pygame.quit()
